70c83e9e488d4b55af62cbc959bb6e58edbbd333
Klaus Peace <klauspeace95@gmail.com>
2022-06-24 16:16:14 +0100

    Add files via upload

---
+++ deploy_Facerec.py
@@ -0,0 +1,29 @@
+import cv2 as cv
+from image_identification import FaceIdentification
+
+# Load Camera
+cap = cv.VideoCapture(0)
+
+# Encode faces from a folder
+mfr = FaceIdentification()
+mfr.load_encoding_images("images/train/")
+
+while True:
+    ret, frame = cap.read()
+    # Detect Faces
+    face_locations, face_names = mfr.detect_known_faces(frame)
+    # Display Face Locations
+    for face_loc, name in zip(face_locations, face_names):
+        y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+
+        cv.putText(frame, name, (x1, y1 - 10), cv.FONT_HERSHEY_DUPLEX, 1, (0,69,255), 2)
+        cv.rectangle(frame, (x1, y1), (x2, y2), (0,0,200), 4)
+
+    cv.imshow('Frame', frame)
+
+    key = cv.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv.destroyAllWindows()
